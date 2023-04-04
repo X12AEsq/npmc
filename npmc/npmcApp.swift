fbbc7f23b040fbf4196866f7a93234b339adb020
Morris Albers <dagger5@alberslegal.com>
2023-04-03 20:18:41 -0500

    Initial Commit

---
+++ npmc/npmcApp.swift
@@ -0,0 +1,17 @@
+//
+//  npmcApp.swift
+//  npmc
+//
+//  Created by Morris Albers on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct npmcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
